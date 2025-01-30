10ef2a3190090adeda7de37e2375578308ab5d49
OMI786I <sohrawardy1998@gmail.com>
2025-01-30 21:38:41 +0600

    skeletal structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,18 +3,18 @@ import { Stack } from "expo-router";
 export default function Layout() {
   return (
     <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
+    // screenOptions={{
+    //   headerStyle: {
+    //     backgroundColor: "#f4511e",
+    //   },
+    //   headerTintColor: "#fff",
+    //   headerTitleStyle: {
+    //     fontWeight: "bold",
+    //   },
+    // }}
     >
       {/* Optionally configure static options outside the route.*/}
-      <Stack.Screen name="index" options={{}} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,69 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text style={styles.headerTitle}>Leader Board</Text>
+        <View style={styles.mainCardContainer}>
+          {[1, 2, 3].map((item, index) => (
+            <View key={index} style={styles.mainCard}>
+              <Image
+                style={styles.mainCardImage}
+                source={{ uri: "" }}
+                resizeMode="cover"
+              />
+              <Text style={styles.mainCardTitle}>Omi</Text>
+              <View style={styles.mainCardRankContainer}>
+                <Text style={styles.mainCardRankContainerTitle}></Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </View>
+      <FlatList
+        data={[1, 2, 3, 4, 5]}
+        renderItem={({ item, index }) => (
+          <View style={styles.card}>
+            <View style={styles.cardDataContainer}>
+              <Text style={styles.cardIndexing}>{index + 1}</Text>
+              <Image
+                style={styles.cardImage}
+                source={{ uri: "" }}
+                resizeMode="cover"
+              />
+              <Text style={styles.cardTitle}>omi</Text>
+            </View>
+            <View style={styles.cardRankContainer}>
+              <Text style={styles.cardRankTitle}>55555</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  topContainer: {},
+  headerTitle: {},
+  mainCard: {},
+  mainCardContainer: {},
+  mainCardImage: {},
+  mainCardTitle: {},
+  mainCardRankContainer: {},
+  mainCardRankContainerTitle: {},
+  card: {},
+  cardIndexing: {},
+  cardImage: {},
+  cardText: {},
+  cardTitle: {},
+  cardDataContainer: {},
+  cardRankContainer: {},
+  cardRankTitle: {},
+});
