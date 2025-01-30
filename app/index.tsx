59faa1bafc53351da922d4c0497acbb344d35f44
OMI786I <sohrawardy1998@gmail.com>
2025-01-30 22:07:06 +0600

    css added

--- app/index.tsx
+++ app/index.tsx
@@ -10,7 +10,7 @@ export default function HomeScreen() {
             <View key={index} style={styles.mainCard}>
               <Image
                 style={styles.mainCardImage}
-                source={{ uri: "" }}
+                source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
                 resizeMode="cover"
               />
               <Text style={styles.mainCardTitle}>Omi</Text>
@@ -29,7 +29,7 @@ export default function HomeScreen() {
               <Text style={styles.cardIndexing}>{index + 1}</Text>
               <Image
                 style={styles.cardImage}
-                source={{ uri: "" }}
+                source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
                 resizeMode="cover"
               />
               <Text style={styles.cardTitle}>omi</Text>
@@ -47,23 +47,79 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#272C35",
+    paddingTop: 40,
+  },
+  topContainer: {
+    backgroundColor: "#1A1F25",
+    paddingTop: 30,
+    padding: 20,
+    borderBottomRightRadius: 40,
+    borderBottomLeftRadius: 40,
+    gap: 20,
+  },
+  headerTitle: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 21,
+    fontWeight: "bold",
+  },
+  mainCard: {
+    backgroundColor: "#272C35",
+    padding: 20,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  mainCardContainer: {
+    flexDirection: "row",
+    gap: 10,
+    justifyContent: "center",
+  },
+  mainCardImage: {
+    width: 50, // Set appropriate width
+    height: 50, // Set appropriate height
+    borderRadius: 5,
+  },
+  mainCardTitle: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: 600,
+  },
+  mainCardRankContainer: {
+    backgroundColor: "orange",
+    width: 30,
+    height: 30,
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 30,
+    position: "absolute",
+    bottom: -15,
   },
-  topContainer: {},
-  headerTitle: {},
-  mainCard: {},
-  mainCardContainer: {},
-  mainCardImage: {},
-  mainCardTitle: {},
-  mainCardRankContainer: {},
-  mainCardRankContainerTitle: {},
-  card: {},
-  cardIndexing: {},
-  cardImage: {},
+  mainCardRankContainerTitle: {
+    color: "white",
+  },
+  card: {
+    padding: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  cardIndexing: {
+    color: "white",
+  },
+  cardImage: { width: 40, height: 40, borderRadius: 50, marginLeft: 10 },
   cardText: {},
-  cardTitle: {},
-  cardDataContainer: {},
+  cardTitle: {
+    color: "white",
+    fontSize: 17,
+  },
+  cardDataContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
   cardRankContainer: {},
-  cardRankTitle: {},
+  cardRankTitle: {
+    color: "orange",
+  },
 });
